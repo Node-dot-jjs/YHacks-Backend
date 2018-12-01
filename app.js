12bd92f335903a1760e85ed276a18b2edaa0c3ce
SamCymbaluk <samuelcymbaluk@gmail.com>
2018-11-30 22:31:21 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const routes = require('./api/routes');
+const port = 80;
+
+dotenv.config();
+
+const app = express();
+const db = require('./api/util/postgres');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', routes(db));
+
+app.listen(port, () => console.log(`Express started on port ${port}`));
